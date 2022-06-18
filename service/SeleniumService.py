4f53f2013727d13d8bb2d532c5fe1a0b68b006cc
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2022-06-19 07:52:34 +0800

    clean up commits update requirements

--- service/SeleniumService.py
+++ service/SeleniumService.py
@@ -1,13 +1,10 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 
 class SeleniumService:
 
-    def __init__(self): 
+    def __init__(self):
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
