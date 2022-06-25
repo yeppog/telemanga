bc3b46b580bd12d78286c9b7c26d1041696b7119
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2022-06-26 02:22:03 +0800

    configure webdriver

--- service/SeleniumService.py
+++ service/SeleniumService.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -8,7 +9,12 @@ class SeleniumService:
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
+        if "GOOGLE_CHROME_PATH" in os.environ: 
+            options.binary_location = os.environ["GOOGLE_CHROME_PATH"]
+        if "CHROME_DRIVER_PATH" in os.environ:
+            self.driver = webdriver.Chrome(os.environ["CHROME_DRIVER_PATH"], chrome_options=options)
+        else:
+            self.driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
 
     def getDriver(self):
         return self.driver
